c1a89228f960ca98c9e3f45ed9ae0da66ae210cc
rocco <rocco.righi@bbc.co.uk>
2016-08-31 17:06:17 +0100

    add chained reaquests

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 exports.render = function(morph) {
-    if (!morph.props.uri) {
+    if (!morph.props.relatedTag) {
         return {
             body: {
                 message: 'You have not specified a name'
@@ -7,16 +7,37 @@ exports.render = function(morph) {
         };
     }
 
-    return morph.request({
-           thing: {
+   function getCurationInfo () { 
+        return morph.request({
+            curation: {
+               uri: {
+                   service: 'curation',
+                   path: '/streammetadata',
+                   query: {'relatedTag': morph.props.relatedTag} 
+               },
+               requireSuccess: true
+            }
+        })
+    };
+
+    function getLdpInfo () { 
+        return morph.request({
+            ldp: {
                uri: {
                    service: 'ldp-core',
                    path: '/tag-concepts',
-                   query: {'uri': morph.props.uri} 
+                   query: {'uri': morph.props.relatedTag} 
                },
                requireSuccess: true
-           }
+            }
         })
-    .then((response) => response.thing);
-};
+    };
 
+    function getCurationMode (response) {
+        return response.curation
+    };
+
+
+    return getCurationInfo()
+        .then((response) => response.curation)
+};
