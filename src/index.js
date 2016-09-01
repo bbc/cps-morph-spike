a1b6734d5bf1e4a4e8a5f0e4cb6326871eaff36c
alinaboghiu <alina.boghiu@gmail.com>
2016-09-01 16:09:53 +0100

    [CPSROADMAP-4792] Successfully processing and combining results from ldp and curation

--- src/index.js
+++ src/index.js
@@ -1,43 +1,47 @@
+function getCurationInfo (morph) {
+    return morph.request({
+        curation: {
+           uri: {
+               service: 'curation',
+               path: '/streammetadata/' + morph.props.tag
+           },
+           requireSuccess: true
+        }
+    })
+};
+
+function getLdpInfo (morph) {
+    return morph.request({
+        ldp: {
+           uri: {
+               service: 'ldp-core',
+               path: '/tag-concepts',
+               query: {'uri': morph.props.tag}
+           },
+           requireSuccess: true
+        }
+    })
+};
+
 exports.render = function(morph) {
-    if (!morph.props.relatedTag) {
+    if (!morph.props.tag) {
         return {
             body: {
-                message: 'You have not specified a name'
+                message: 'You have not specified a tag'
             }
         };
     }
 
-   function getCurationInfo () { 
-        return morph.request({
-            curation: {
-               uri: {
-                   service: 'curation',
-                   path: '/streammetadata',
-                   query: {'relatedTag': morph.props.relatedTag} 
-               },
-               requireSuccess: true
+    return Promise.all([getCurationInfo(morph), getLdpInfo(morph)])
+        .then((responses) => {
+            var curation = responses[0].curation.body
+            var ldp = responses[1].ldp.body
+
+            var curationMode = JSON.parse(curation)["curationMode"]
+            var labels = JSON.parse(ldp)["results"][0]["core:label"]
+
+            return {
+                body: {curation: curationMode, ldp: labels}
             }
         })
-    };
-
-    function getLdpInfo () { 
-        return morph.request({
-            ldp: {
-               uri: {
-                   service: 'ldp-core',
-                   path: '/tag-concepts',
-                   query: {'uri': morph.props.relatedTag} 
-               },
-               requireSuccess: true
-            }
-        })
-    };
-
-    function getCurationMode (response) {
-        return response.curation
-    };
-
-
-    return getCurationInfo()
-        .then((response) => response.curation)
 };
